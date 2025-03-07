26519f9714e3824099ee9a49923140e24510edc2
Belolipetsky <mefistofi098@gmail.com>
2025-03-08 00:30:56 +0300

    Update session_keyboard.py

--- keyboards/session_keyboard.py
+++ keyboards/session_keyboard.py
@@ -1,7 +1,7 @@
 from aiogram import types
 
 def get_session_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add("🎓 Записаться на личную сессию")
     keyboard.add("🔄 Подумать позже")
     return keyboard
