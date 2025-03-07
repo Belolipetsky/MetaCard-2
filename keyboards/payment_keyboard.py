1cfcba5ca481b3ca7909baa89cf3880e41f9c5f8
Belolipetsky <mefistofi098@gmail.com>
2025-03-08 00:30:28 +0300

    Update payment_keyboard.py

--- keyboards/payment_keyboard.py
+++ keyboards/payment_keyboard.py
@@ -1,8 +1,10 @@
-from aiogram import types
-
-def get_payment_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    keyboard.add("Купить 1 карту – 89₽")
-    keyboard.add("Купить 5 карт – 356₽ (Экономия 20%)")
-    keyboard.add("Купить 10 карт – 699₽ (Самая выгодная цена!)")
-    return keyboard
+# Пока функционал оплаты не используется в MVP, код ниже закомментирован.
+#
+# from aiogram import types
+#
+# def get_payment_keyboard():
+#     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     keyboard.add("Купить 1 карту – 89₽")
+#     keyboard.add("Купить 5 карт – 356₽")
+#     keyboard.add("Купить 10 карт – 699₽")
+#     return keyboard
