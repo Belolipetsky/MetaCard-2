5f73910ba283e297dd892b31b5c2c72cb90689b4
Belolipetsky <mefistofi098@gmail.com>
2025-03-08 00:30:11 +0300

    Update main_menu.py

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -2,5 +2,6 @@ from aiogram import types
 
 def get_main_menu_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add("🔮 Вытянуть карту", "🎓 Записаться на сессию")
+    keyboard.add("🔮 Вытянуть карту")
+    keyboard.add("🎓 Записаться на сессию")
     return keyboard
